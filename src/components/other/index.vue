fd75d889ff0c0c050ec6294324bbea6a2537680a
cpx <176156562@qq.com>
2019-03-26 16:05:54 +0800

    modifyall

--- src/components/other/index.vue
+++ src/components/other/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="other">
         this is other page {{count}}
+        <div>{{message}}</div>
     </div>
 </template>
 
@@ -8,10 +9,43 @@
 import {index} from '../../store';
 export default {
     name: 'Other',
-    data(){
+    data() {
         return {
-            count:index.state.count
+            count: index.state.count,
+            message: 'Vue的生命周期'
         };
+    },
+    beforeCreate: function() {
+        const text = '------beforeCreate创建前状态------';
+        console.log('','color:orange',text);
+    },
+    created: function() {
+        const text = '------created创建完毕状态------';
+        console.log('','color:orange',text);
+    },
+    beforeMount: function() {
+        const text = '------beforeMount挂载前状态------';
+        console.log('','color:orange',text);
+    },
+    mounted: function() {
+        const text = '------mounted 挂载结束状态------';
+        console.log('','color:orange',text);
+    },
+    beforeUpdate: function () {
+        const text = '------beforeUpdate 更新前状态------';
+        console.log('','color:orange',text);
+    },
+    updated: function () {
+        const text = '------updated 更新完成状态------';
+        console.log('','color:orange',text);
+    },
+    beforeDestroy: function () {
+        const text = '------beforeDestroy 销毁前状态------';
+        console.log('','color:orange',text);
+    },
+    destroyed: function () {
+        const text = '------destroyed 销毁完成状态------';
+        console.log('','color:orange',text);
     }
 }
 </script>
